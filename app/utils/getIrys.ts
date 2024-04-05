32e61cea1890594ca3be6c3e416beacd9c485126
pinky <pinky@dev.com>
2024-04-05 20:05:10 +0200

    refactor: Seperate Connect Wallet function

--- app/utils/getIrys.ts
+++ app/utils/getIrys.ts
@@ -1,5 +1,6 @@
 import { ethers } from "ethers";
 import { WebIrys } from "@irys/sdk";
+import WalletConnect from "./WalletConnect";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -17,40 +18,37 @@ dotenv.config();
 
 // const [connectedAddress, setConnectedAddress] = useState<string>();
 const getWebIrys = async () => {
-  if (typeof window.ethereum !== "undefined") {
-    await window.ethereum.enable();
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    console.log("provider=>", provider);
-    const signer = provider.getSigner();
-    //   setConnectedAddress(signer);
-    console.log("Signer: ", signer);
-    const blockNr = await provider.getBlockNumber();
-    console.log("blockNr=>", blockNr);
+  const provider = await WalletConnect();
+  console.log("provider", provider);
+  // if (typeof window.ethereum !== "undefined") {
+  //   await window.ethereum.enable();
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //   console.log("provider=>", provider);
 
-    const url = "https://devnet.irys.xyz";
-    console.log("url=>", url);
-    const token = "arbitrum";
-    console.log("token=>", token);
-    //   const rpcURL = new providers.Web3Provider((window as WindowWithEthereum).ethereum);
-    const rpcURL = process.env.NEXT_PUBLIC_RPC_ARBITRUM;
-    console.log("providerUrl=>", rpcURL);
+  const url = "https://devnet.irys.xyz";
+  console.log("url=>", url);
+  const token = "arbitrum";
+  console.log("token=>", token);
+  //   const rpcURL = new providers.Web3Provider((window as WindowWithEthereum).ethereum);
+  const rpcURL = process.env.NEXT_PUBLIC_RPC_ARBITRUM;
+  console.log("providerUrl=>", rpcURL);
 
-    // Create a wallet object
-    const wallet = { rpcUrl: rpcURL, name: "ethersv5", provider: provider };
-    console.log("wallet=>", wallet);
+  // Create a wallet object
+  const wallet = { rpcUrl: rpcURL, name: "ethersv5", provider: provider };
+  console.log("wallet=>", wallet);
 
-    console.log("connect to webIrys");
-    // Use the wallet object
-    const webIrys = new WebIrys({ url, token, wallet });
-    console.log("webIrys=>", webIrys);
+  console.log("connect to webIrys");
+  // Use the wallet object
+  const webIrys = new WebIrys({ url, token, wallet });
+  console.log("webIrys=>", webIrys);
 
-    console.log("connect to WebIrys.ready");
-    await webIrys.ready();
-    console.log("connected to WebIrys.ready");
+  console.log("connect to WebIrys.ready");
+  await webIrys.ready();
+  console.log("connected to WebIrys.ready");
 
-    console.log(`Connected to webIrys from ${webIrys.address}`);
-    return webIrys;
-  }
+  console.log(`Connected to webIrys from ${webIrys.address}`);
+  return webIrys;
+  // }
 };
 // }
 
